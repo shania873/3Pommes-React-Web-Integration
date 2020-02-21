66189161feae0cd2d4b9f0983798c4ee59936f33
Caroline van Aerschot <vanaeca@hotmail.com>
2020-02-21 09:59:56 +0100

    premier commit

---
+++ src/scripts/index.js
@@ -0,0 +1,74 @@
+import '../styles/index.scss';
+import $ from '../../node_modules/jquery/src/jquery.js';
+window.jQuery = $;
+window.$ = $;
+import boostrap from 'bootstrap';
+import './slick.js';
+// import '../../node_modules/jquery-ui/';
+var arrow = document.querySelectorAll("slick-arrow");
+
+arrow.innerHTML = "";
+
+function btnRappelsMessages(){
+    var btnRappels = document.getElementById("btn--rappels");
+    var btnMessages = document.getElementById("btn--messages");   
+    
+    var containerRappels = document.getElementById("rappels--partBienvenue");
+    var containerMessages = document.getElementById("messages--partBienvenue");
+    
+    var titre = document.getElementById("titre");
+    
+    containerRappels.style.display = "none";
+    // titre.innerHTML = "Rappels";
+    
+    btnRappels.addEventListener("click", function () {
+        containerMessages.style.display = "block";
+        containerRappels.style.display = "none";       
+        titre.innerHTML = "Messages";
+    
+        
+    });
+    btnMessages.addEventListener("click", function () {
+    
+      
+
+
+        
+        containerMessages.style.display = "none";
+        containerRappels.style.display = "block";      
+        titre.innerHTML = "Rappels";
+        
+    });
+    
+}
+function diaporama(){
+    $('.card-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 1500,
+        arrow: true,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+  
+}
+
+function init(){
+    btnRappelsMessages();
+    diaporama();
+}
+
+
+init();
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,56 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js',
+    path: Path.resolve(__dirname, '../public'),
+  },
+  devServer: {  
+    // contentBase: Path.resolve(__dirname, '../public'),
+    inline: true,  
+  },
+  plugins: [ 
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        options: {
+          emitWarning: true,
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|jpeg|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+             name:'[path][name].[ext]',
+
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader?sourceMap=true', 'sass-loader']
+      }
+    ]
+  }
+});
